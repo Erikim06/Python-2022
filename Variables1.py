c948e7168dbab4edebc3e9bb5964783240a190c6
Erikim06 <erikim06@kingshigh.school.nz>
2022-03-03 09:59:35 +1300

    variable1

---
+++ Variables1.py
@@ -0,0 +1,17 @@
+player_lives=3#1
+player_name=Tom#2
+player_money=800000#3
+
+phone_number = input("what is your phone number?")#4
+user_name = input("what is your name?")#5
+user_age = input("How old are you?")#6
+year = input("what's the year?")#7
+print(f"your phone number is {phone_number}")
+print(f"your name is {user_ame}")
+print(f"your age is {user_age}")
+print(f"the year is {year}")
+
+user_surname =input("what is your sur name?")
+user_school =input("what is your school name?")
+user_faverite food =input("what is your favouite food?")
+	
