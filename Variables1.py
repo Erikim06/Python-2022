5cd71f169bf292d01b49dcd451f8beeb7c666402
Erikim06 <erikim06@kingshigh.school.nz>
2022-03-04 11:00:10 +1300

    variables2

--- Variables1.py
+++ Variables1.py
@@ -1,17 +1,24 @@
 player_lives=3#1
-player_name=Tom#2
+player_name="Tom"#2
 player_money=800000#3
 
 phone_number = input("what is your phone number?")#4
 user_name = input("what is your name?")#5
 user_age = input("How old are you?")#6
 year = input("what's the year?")#7
-print(f"your phone number is {phone_number}")
-print(f"your name is {user_ame}")
-print(f"your age is {user_age}")
-print(f"the year is {year}")
+print(f"your phone number is {phone_number}")#8
+print(f"your name is {user_name}")#9
+print(f"your age is {user_age}")#10
+print(f"the year is {year}")#11
 
-user_surname =input("what is your sur name?")
-user_school =input("what is your school name?")
-user_faverite food =input("what is your favouite food?")
-	
+user_surname = input("what is your sur name?")#12
+user_school = input("what is your school name?")#13
+user_favourite_food = input("what is your favourite food?")#14
+user_pet = input("what is your pet name?")#15	
+blocks_of_chocolate = 5 #16
+players_score = 100 #17
+players_highest_score = 200 #18
+players_number_of_laps = 1 #19
+ammunition_left = 8 #20
+current_bear = 8 #21
+players_current speed = 300 #22 
---
+++ Variables2.py
@@ -0,0 +1,11 @@
+money = 200
+hat = 20
+top = 30
+pants = 15
+belt = 60
+shoes = 40
+print(f"you have{money}")
+print("buying a hat")
+print(money-hat)
+print("you now have the following amount left over")
+print(money)
