2bf59445ffade823e632b56e1b7a63d568fcf5f3
Erikim06 <erikim06@kingshigh.school.nz>
2022-03-07 12:25:25 +1300

    exercise

--- Variables2.py
+++ Variables2.py
@@ -9,3 +9,28 @@ print("buying a hat")
 print(money-hat)
 print("you now have the following amount left over")
 print(money)
+
+print(f"you have{money}")
+print("buying a top")
+print(money-top)
+print("you now have the following amount left over")
+print(money)
+
+print(f"you have{money}")
+print("buying a pants")
+print(money-pants)
+print("you now have the following amount left over")
+print(money)
+
+print(f"you have{money}")
+print("buying a belt")
+print(money-belt)
+print("you now have the following amount left over")
+print(money)
+
+
+print(f"you have{money}")
+print("buying a shoes")
+print(money-shoes)
+print("you now have the following amount left over")
+print(money)
