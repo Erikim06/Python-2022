cc6fec8fc78222f1b149c64096558fbef5cdb3c3
Erikim06 <erikim06@kingshigh.school.nz>
2022-03-11 09:59:20 +1300

    Update exercises2.py

--- exercises2.py
+++ exercises2.py
@@ -10,9 +10,28 @@ if how_far >200:
     print("fill up the petrol before you travel")
 elif how_far <200:
     print("goodgood")
-"""
+
 user_age = int(input("what is your age?")) #3
 if user_age >= 18:
     print("you are an adult!")
 elif user_age < 18:
 	print("you are a kid")
+
+user_movie = input("What is your favourite movie?") #4
+if user_movie =="Lord of the king":
+    print("you have exellent taste!")
+else:
+    print("Lord of the kings is better")
+
+
+user_heard =input("have you heard the tale of Darvh Plagueis the Wise?") #5
+if user_heard == "yes":
+	print(" You must be a fan")
+else:
+	print("tale of Darvh Plagueis the Wise")
+"""
+Director =input("who is the director of Passion of the Christ?") #6
+if Director == "Mel Gibson":
+	print("Correct")
+else:
+	print("unlucky")
