68152c1cd4bb5e06533f14d3ce3d5ade3f659adb
Erikim06 <erikim06@kingshigh.school.nz>
2022-03-14 10:55:51 +1300

    list.py

---
+++ list.py
@@ -0,0 +1,6 @@
+fibonacci= (0, 1, 1, 2, 3, 5, 8, 13, 21, 34) #1
+print(fibbonacci)
+fruit= ("mongo, grape, melon, finnnn, tomato") #2
+print(fruit)
+youtubers= ("Blackpink, Aespa, Twice, BTS, Ive") #3
+print(youtubers)
