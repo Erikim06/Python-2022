386827c003faa8909e271ca7ff10629f17e3646d
Erikim06 <erikim06@kingshigh.school.nz>
2022-03-15 12:24:56 +1300

    list.py

--- list.py
+++ list.py
@@ -1,6 +1,45 @@
+"""
 fibonacci= (0, 1, 1, 2, 3, 5, 8, 13, 21, 34) #1
-print(fibbonacci)
+print(fibonacci)
 fruit= ("mongo, grape, melon, finnnn, tomato") #2
 print(fruit)
 youtubers= ("Blackpink, Aespa, Twice, BTS, Ive") #3
 print(youtubers)
+music= []
+music.append("Dreams come true")#4
+music.append("Boy with luv")
+music.append("Feel special")
+music.append("How you like that")
+music.append("As if its your last")
+print(music)
+
+books= []
+books.append("Harry Poter")#5
+books.append("Why K-pop best")
+books.append("Lee Child")
+books.append("The room")
+books.append("The kite runner")
+print(books)
+"""
+pizza_toppings= []
+pizza= input("Add a pizza topping") #6
+if pizza != "":
+	pizza_toppings.append(pizza)
+pizza= input("Add a pizza topping")
+if pizza != "":
+	pizza_toppings.append(pizza)
+pizza= input("Add a pizza topping")
+if pizza != "":
+	pizza_toppings.append(pizza)
+pizza= input("Add a pizza topping")
+if pizza != "":
+	pizza_toppings.append(pizza)
+pizza= input("Add a pizza topping")
+if pizza != "":
+	pizza_toppings.append(pizza)
+pizza= input("Add a pizza topping")
+if pizza != "":
+	pizza_toppings.append(pizza)
+print(pizza_toppings)	
+
+fruit= ["mango", "grape", "melon"]
