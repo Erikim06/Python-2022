728989a51607ab8e4d74f2af11eb9ce6c9e8cb4b
Erikim06 <erikim06@kingshigh.school.nz>
2022-03-28 14:59:59 +1300

    Update text_advanture.py

--- text_advanture.py
+++ text_advanture.py
@@ -1,1 +1,46 @@
 from adventurelib import *
+
+Room.items = Bag()
+
+room1 = Room("You are awake in this bedroom, it's old, dark abondened room. You can get the key in this room ")
+room2 = Room("The room2 is locked. You have to use the item to unlock it. When you get there you may get chain saw ")
+kitchen = Room("The kitchen is is very simple and eveything is dark, you can get the key there.")
+room3 = Room("The room3 is very small and there are nothing in there.")
+room4 = Room("The room4 is a living room and you need the key from the Room1 to open the secret room")
+corridor = Room("Corridor is where the front door is, get all the items and try to unlock.")
+prison = Room("The prison is just horrible, there are bonds everywhere and the weapons. You can get the hammer ")
+master_room = Room("master room is look alright and you can get the driver from there")
+
+
+room2.north = corridor
+room2.south = kitchen
+kitchen.east = master_room
+master_room.north = prison
+prison.west = room4
+room4.west = corridor
+
+
+Item.description = ""
+
+key1 = Item("Shiny silver key")
+key1.description = "You use it to escape the first room and to unlock the secret room."
+
+key2 = Item("Rust key")
+key2.description = "You use it to unlock the prison"
+
+driver = Item("Normal driver")
+driver.description = "The driver is to help you to unlock the front door. "
+
+hammer = Item("The wood hammer")
+hammer.description = "The hammer is to unlock the front door"
+
+chain_saw = Item("The chain saw")
+cahin_saw.description = "it is for unlock the front door"
+
+flare_gun = Item("Flare gun")
+flare_gun.description= "You have to shoot the flare gun for help."
+def main():
+	start()
+
+if __name__ == '__main__':
+	main()
