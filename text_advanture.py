159584a01adbbf537cafa030864e3bf9162b0484
Erikim06 <erikim06@kingshigh.school.nz>
2022-03-31 10:59:46 +1300

    Update text_advanture.py

--- text_advanture.py
+++ text_advanture.py
@@ -12,6 +12,7 @@ prison = Room("""The prison is just horrible, there are bonds everywhere and the
 master_room = Room("""master room is look alright and you can get the driver from there""")
 
 
+
 room2.north = corridor
 room2.south = kitchen
 kitchen.east = master_room
@@ -32,13 +33,13 @@ driver = Item("Normal driver")
 driver.description = "The driver is to help you to unlock the front door. "
 
 hammer = Item("The wood hammer")
-hammer.description = "The hammer is to unlock the front door"
+hammer.description = "The hammer is to unlock the front door."
 
 chain_saw = Item("The chain saw")
-cahin_saw.description = "it is for unlock the front door"
+chain_saw.description = "it is for unlock the front door."
 
 flare_gun = Item("Flare gun")
-flare_gun.description= "You have to shoot the flare gun for help after you escape."
+flare_gun.description= "You have to shoot the flare gun for help after you escape the house."
 
 
 room1.items.add(key1)
@@ -53,16 +54,41 @@ room4.add(flare_gun)
 current_room = space
 inventory = Bag()
 
+
+@when("use chain saw")
+@when("use driver")
+def escape(house):
+	if current_room == corridor:
+		chain_saw or driver in inventory
+		print("You succesed to escape!")
+	else:
+		print("You can't use it")
+
+
+
 @when("get ITEM")
 @when("take ITEM")
 @when("pick up ITEM")
-def = pickup(item):
-	if current_room = 
+def pickup(item):
+	if item in current_room.items:
+		t = current_room.items.take(item)
+		inventory.add(t)
+		print(f"You pick up the {item}")
+	else:
+		print(f"you don't see a {item}")
 
+@when ("go DIRECTION")
+def travel(direction):
+	global current_room
+	if direction in current_room.exits():
+		current_room = current_room.exit(direction)
+		print(f"you go {direction}.")
+		print(current_room)
 
 
 @when("look at ITEM")
-@when("my Item")
+@when("My ITEM")
+@when("my ITEM")
 def look_at(item):
 	if item in inventory:
 		t = inventory.find(item)
